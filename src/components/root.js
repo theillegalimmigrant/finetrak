a309ef70d0b4560e63f20ee79024301c8503ba87
Nathan Ching <theillegalimmigrant@gmail.com>
2018-08-09 10:56:07 +1000

    Adding firebase init

--- src/components/root.js
+++ src/components/root.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react'
 import {browserHistory} from 'react-router'
+import {init as firebaseInit} from 'javascript/firebase'
 import Routes from './routes'
 class Root extends Component {
   constructor(props) {
     super(props)
+    firebaseInit();
   }
 render() {
     return (
---
+++ src/javascript/firebase.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/5.3.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDTV6dUnBozzUNjdwwyR0XUgW1YC2gJexU",
+    authDomain: "finetrak.firebaseapp.com",
+    databaseURL: "https://finetrak.firebaseio.com",
+    projectId: "finetrak",
+    storageBucket: "finetrak.appspot.com",
+    messagingSenderId: "1040720117676"
+  };
+  firebase.initializeApp(config);
+</script>
